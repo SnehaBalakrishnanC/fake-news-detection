2187e3fdc5b1c887d7d8dc9a3a9da4ac861177a8
neharaveendran <nehapathiyamboor@gmail.com>
2023-03-09 02:34:57 -0800

    import

--- fakenewsdetection.py
+++ fakenewsdetection.py
@@ -1,1 +1,20 @@
 !unzip /content/drive/MyDrive/FakeNewsDataset/FakeNewsDataset.zip
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import nltk
+import re
+import string 
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+
+import keras
+from keras.preprocessing import text,sequence
+from keras.models import Sequential
+from keras.layers import Dense,Embedding,LSTM,Dropout
+
+import warnings
+warnings.filterwarnings('ignore')
