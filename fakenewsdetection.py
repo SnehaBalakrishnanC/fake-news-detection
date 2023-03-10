abd396b3572d6fb91c51fa5983dd221cf68607c8
Avani-Rajashekar <avaninambiar2001@gmail.com>
2023-03-10 12:01:53 +0530

    import

--- fakenewsdetection.py
+++ fakenewsdetection.py
@@ -66,3 +66,9 @@ first_text = data.text[10]
 first_text
 
 pip install bs4
+
+from bs4 import BeautifulSoup
+
+soup = BeautifulSoup(first_text, "html.parser")
+first_text = soup.get_text()
+first_text
